e5d706b55b5ffff956da1dd5a7775d0e55d0117c
martin61 <martin61@users.noreply.github.com>
2016-01-24 12:48:29 +0100

    Update verify.go
    martin61

--- cmd/verify.go
+++ cmd/verify.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	"github.com/MDrollette/i2p-tools/reseed"
-	"github.com/MDrollette/i2p-tools/su3"
+	"github.com/martin61/i2p-tools/reseed"
+	"github.com/martin61/i2p-tools/su3"
 	"github.com/codegangsta/cli"
 )
 
